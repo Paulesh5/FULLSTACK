cd0783b1f79b1f079688769954ba88004b66443d
Paul Hidalgo <paulesh5@gmail.com>
2024-02-10 18:18:46 -0500

    Sprint 0

--- frontend-veterinaria/src/paginas/Restablecer.jsx
+++ frontend-veterinaria/src/paginas/Restablecer.jsx
@@ -4,9 +4,27 @@ import Mensaje from '../componets/Alertas/Mensaje'
 import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+
+
 
 
 const Restablecer = () => {
+
+    const navigate = useNavigate();
+
+    const [form, setForm] = useState({
+            password:"",
+            confirmpassword:""
+        })
+    
+    const handleChange = (e) => {
+            setForm({
+                ...form,
+                [e.target.name]: e.target.value
+            })
+        }
+
     const { token } = useParams();
     const [mensaje, setMensaje] = useState({})
 		const [tokenback, setTokenBack] = useState(false)
@@ -26,18 +44,50 @@ const Restablecer = () => {
     }, [])
 
 
+    const handleSubmit = async(e) => {
+        e.preventDefault()
+        try {
+            const url = `${import.meta.env.VITE_BACKEND_URL}/nuevo-password/${token}`
+            const respuesta = await axios.post(url,form)
+            setForm({})
+            setMensaje({respuesta:respuesta.data.msg,tipo:true})
+						setTimeout(() => {
+                navigate('/login');
+            }, 3000);
+        } catch (error) { 
+            setMensaje({respuesta:error.response.data.msg,tipo:false})
+        }
+    }
+
     return (
         <div className="flex flex-col items-center justify-center">
-
+            {Object.keys(mensaje).length > 0 && <Mensaje tipo={mensaje.tipo}>{mensaje.respuesta}</Mensaje>}
+			<h1 className="text-3xl font-semibold mb-2 text-center uppercase  text-gray-500">Welcome again</h1>
+            <small className="text-gray-400 block my-4 text-sm">Please enter your details</small>
             <img className="object-cover h-80 w-80 rounded-full border-4 border-solid border-slate-600" src={logoDog} alt="image description" />
-
-            <div className="flex flex-col items-center justify-center">
-                {Object.keys(mensaje).length>0 && <Mensaje tipo={mensaje.tipo}>{mensaje.respuesta}</Mensaje>}
-                <p className="md:text-lg lg:text-xl text-gray-600 mt-8">Ya puedes iniciar sesión</p>
-                <Link to="/login" className="p-3 m-5 w-full text-center bg-gray-600 text-slate-300 border rounded-xl hover:scale-110 duration-300 hover:bg-gray-900 hover:text-white">Login</Link>
-            </div>
-
-        </div>
+            {tokenback &&
+                <form className='w-full' onSubmit={handleSubmit}>
+                <div className="mb-1">
+                    <label className="mb-2 block text-sm font-semibold">Password</label>
+                    <input type="password" placeholder="Enter your password" className="block w-full rounded-md border border-gray-300 focus:border-purple-700 focus:outline-none focus:ring-1 focus:ring-purple-700 py-1 px-1.5 text-gray-500"
+                      value={form.password || ""}
+                      name='password'
+                      onChange={handleChange}
+                    />
+                    <label className="mb-2 block text-sm font-semibold">Confirm password</label>
+                    <input type="password" placeholder="Repeat your password" className="block w-full rounded-md border border-gray-300 focus:border-purple-700 focus:outline-none focus:ring-1 focus:ring-purple-700 py-1 px-1.5 text-gray-500"
+                      value={form.confirmpassword || ""}
+                      name='confirmpassword'
+                      onChange={handleChange}
+                    />
+                </div>
+                <div className="mb-3">
+                  <button className="bg-gray-600 text-slate-300 border py-2 w-full rounded-xl mt-5 hover:scale-105 duration-300 hover:bg-gray-900 hover:text-white">Send
+                  </button>
+                </div>
+            </form>
+        }
+    </div>
     )
 }
 
