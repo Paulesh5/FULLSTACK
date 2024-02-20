20085496c497b20e61016cb79247cc64d3e6774a
Paulesh5 <paulesh5@gmail.com>
2024-02-20 15:49:12 -0500

    Módulo Veterinario

--- frontend-veterinaria/src/context/AuthProvider.jsx
+++ frontend-veterinaria/src/context/AuthProvider.jsx
@@ -37,13 +37,52 @@ const AuthProvider = ({ children }) => {
         }
     }, [])
     
+    const actualizarPerfil = async(datos) => {
+        const token = localStorage.getItem('token')
+        try {
+            const url = `${import.meta.env.VITE_BACKEND_URL}/veterinario/${datos.id}`
+            const options = {
+                headers: {
+                    method: 'PUT',
+                    'Content-Type': 'application/json',
+                    Authorization: `Bearer ${token}`
+                }
+            }
+            const respuesta = await axios.put(url, datos, options)
+            perfil(token)
+            return {respuesta:respuesta.data.msg,tipo:true}
+        } catch (error) {
+            return {respuesta:error.response.data.msg,tipo:false}
+        }
+    }
+
+    const actualizarPassword = async (datos) => {
+        const token = localStorage.getItem('token')
+        try {
+            const url = `${import.meta.env.VITE_BACKEND_URL}/veterinario/actualizarpassword`
+            const options = {
+                headers: {
+                    method: 'PUT',
+                    'Content-Type': 'application/json',
+                    Authorization: `Bearer ${token}`
+                }
+            }
+            const respuesta = await axios.put(url, datos, options)
+            return { respuesta: respuesta.data.msg, tipo: true }
+        } catch (error) {
+            return { respuesta: error.response.data.msg, tipo: false }
+        }
+    }
+
     // Retornar el contenido del mensaje
     return (
         <AuthContext.Provider value={
             //El mensaje a ser enviado
             {
                 auth,
-                setAuth              
+                setAuth,
+                actualizarPerfil,
+                actualizarPassword              
             }
         }>
             {children}
